58c9fae489c39afdb35e86f2b256f053a223e968
Johannes Stephan Przymusinski <johannes.przymusinski@jop-software.de>
2020-05-24 21:41:31 +0200

    add register link to login pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -57,6 +57,8 @@
                                     {{ __('Login') }}
                                 </button>
 
+                                <a href="/register" class="btn btn-primary">{{ __("Register") }}</a>
+
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
